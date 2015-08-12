4d6f3436437b9f641b4e58a5d8f03f6f8b5cc814
Justin Pealing <justin@pealing.me.uk>
2015-08-12 21:57:23 +0100

    Fixed pyHook + sound loading on linux
    All sounds in Sounds directory now loaded with correct path separator for OS.
    
    Errors loading pyHook are ignored as this probably indicates running on linux where it is not required

--- Main.py
+++ Main.py
@@ -1,39 +1,34 @@
 import pygame
 import random
-import pyHook
+from os import listdir
+from os.path import join
 
-# http://code.activestate.com/recipes/553270-using-pyhook-to-block-windows-keys/
-def OnKeyboardEvent(event):
-	if event.Key.lower() in ['lwin', 'rwin']:
-		return False
-	else:
-		return True
+try:
+	import pyHook
 
-hm = pyHook.HookManager()
-hm.KeyDown = OnKeyboardEvent
-hm.HookKeyboard()
+	# http://code.activestate.com/recipes/553270-using-pyhook-to-block-windows-keys
+	def OnKeyboardEvent(event):
+	       if event.Key.lower() in ['lwin', 'rwin']:
+	               return False
+	       else:
+	               return True
+
+	hm = pyHook.HookManager()
+	hm.KeyDown = OnKeyboardEvent
+	hm.HookKeyboard()
+except:
+	pass
 
 pygame.init()
-screen = pygame.display.set_mode((640, 480))
 
 effects = []
-effects.append(pygame.mixer.Sound('Sounds\low-intesity-a3.wav'))
-effects.append(pygame.mixer.Sound('Sounds\low-intesity-a3.wav'))
-effects.append(pygame.mixer.Sound('Sounds\low-intesity-a4.wav'))
-effects.append(pygame.mixer.Sound('Sounds\low-intesity-b3.wav'))
-effects.append(pygame.mixer.Sound('Sounds\low-intesity-c3.wav'))
-effects.append(pygame.mixer.Sound('Sounds\low-intesity-c4.wav'))
-effects.append(pygame.mixer.Sound('Sounds\low-intesity-d3.wav'))
-effects.append(pygame.mixer.Sound('Sounds\low-intesity-d4.wav'))
-effects.append(pygame.mixer.Sound('Sounds\low-intesity-e3.wav'))
-effects.append(pygame.mixer.Sound('Sounds\low-intesity-e4.wav'))
-effects.append(pygame.mixer.Sound('Sounds\low-intesity-f3.wav'))
-effects.append(pygame.mixer.Sound('Sounds\low-intesity-f4.wav'))
-effects.append(pygame.mixer.Sound('Sounds\low-intesity-g3.wav'))
-effects.append(pygame.mixer.Sound('Sounds\low-intesity-g4.wav'))
+for f in listdir("Sounds"):
+	p = join("Sounds", f)
+	effects.append(pygame.mixer.Sound(p))
+
+screen = pygame.display.set_mode((640, 480))
 
 running = 1
-
 while running:
 	event = pygame.event.poll()
 	if event.type == pygame.QUIT:
@@ -41,6 +36,3 @@ while running:
 	if event.type == pygame.KEYDOWN:
 		r = random.randint(0, len(effects) - 1)
 		effects[r].play()
-
-	screen.fill((0, 0, 0))
-	pygame.display.flip()
